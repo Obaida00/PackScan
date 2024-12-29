eb11bebe61f93617df5c26ed6fac3c5b73fc9a39
Obaida Almelhem <almelhemo@gmail.com>
2024-12-29 20:50:34 +0300

    added missing react imports

--- frontend/src/components/TableComponents/Packing/Decrement.jsx
+++ frontend/src/components/TableComponents/Packing/Decrement.jsx
@@ -1,14 +1,16 @@
+import * as React from "react";
+
 function Decrement({ data, action }) {
-    return (
-        <div className="w-full flex">
-            <button
-                className="w-full rounded-full bg-red-500 hover:bg-red-700 opacity-40 hover:opacity-100 font-serif font-black transition-all duration-100"
-                onClick={() => action(data.barcode)}
-            >
-                -
-            </button>
-        </div>
-    );
+  return (
+    <div className="w-full flex">
+      <button
+        className="w-full rounded-full bg-red-500 hover:bg-red-700 opacity-40 hover:opacity-100 font-serif font-black transition-all duration-100"
+        onClick={() => action(data.barcode)}
+      >
+        -
+      </button>
+    </div>
+  );
 }
 
 export default Decrement;
