342591c4e02eec20734618fa53d00615401445ff
Obaida Almelhem <almelhemo@gmail.com>
2025-01-01 00:30:48 +0300

    refactoring axios code

--- frontend/src/axios-client.js
+++ frontend/src/axios-client.js
@@ -5,52 +5,48 @@ const axios = require("axios");
 // });
 
 export async function fetchAllInvoices(pageNumber = 1) {
-  try {
-    const response = await axios.get(
-      `http://127.0.0.1:8000/api/invoices?page=${pageNumber}`
-    );
-    return response.data;
-  } catch (error) {
-    throw error.data;
-  }
+  const response = await axios
+    .get(`http://127.0.0.1:8000/api/invoices?page=${pageNumber}`)
+    .catch((error) => {
+      throw error;
+    });
+  return response.data;
 }
 
 export async function getBySearchStorageInvoices(storageCode, input) {
-  try {
-    const response = await axios.get(
+  const response = await axios
+    .get(
       `http://127.0.0.1:8000/api/invoices?st[eq]=${storageCode}&id[li]=${input}%`
-    );
-    return response.data;
-  } catch (error) {
-    throw error.data;
-  }
+    )
+    .catch((error) => {
+      throw error;
+    });
+  return response.data;
 }
 
 export async function getInvoiceById(id) {
-  try {
-    const response = await axios.get(
-      `http://127.0.0.1:8000/api/invoices/${id}`
-    );
-    return response.data;
-  } catch (error) {
-    throw error.data;
-  }
+  const response = await axios
+    .get(`http://127.0.0.1:8000/api/invoices/${id}`)
+    .catch((error) => {
+      throw error;
+    });
+  return response.data;
 }
 
 export async function submitInvoice(id) {
-  try {
-    await axios.post(`http://127.0.0.1:8000/api/invoices/${id}/done`);
-  } catch (error) {
-    throw error.data;
-  }
+  await axios
+    .post(`http://127.0.0.1:8000/api/invoices/${id}/done`)
+    .catch((error) => {
+      throw error;
+    });
 }
 
 export async function uploadNewInvoice(data) {
-  try {
-    await axios.post(`http://127.0.0.1:8000/api/invoices`, data, {
+  await axios
+    .post(`http://127.0.0.1:8000/api/invoices`, data, {
       headers: { "Content-Type": "application/json" },
+    })
+    .catch((error) => {
+      throw error;
     });
-  } catch (error) {
-    throw error.data;
-  }
 }
