86b7216c76a693af35d136323d760b041c7655f2
Obaida Almelhem <almelhemo@gmail.com>
2025-01-02 20:20:23 +0300

    fix bug logo not showing up

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Logo from "../assets/images/Logo.svg";
 
-function Header({theme, themeToggle}) {
+function Header() {
+  var logoPath = Logo;
 
   return (
     <nav className="max-h-[10vh] bg-gray-800">
       <div className="max-w-screen-xl flex justify-between mx-auto p-[2vh]">
         <div className="flex space-x-2">
-          <img className="h-[6vh]" alt="" src="/src/assets/Logo.svg"></img>
+          <img className="h-[6vh]" alt="" src={logoPath}></img>
           <p className="self-center text-2xl whitespace-nowrap font-cocon text-white">
             PackScan
           </p>
--- frontend/webpack.rules.js
+++ frontend/webpack.rules.js
@@ -1,19 +1,19 @@
-const path = require('path');
+const path = require("path");
 module.exports = [
   // Add support for native node modules
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
     test: /native_modules[/\\].+\.node$/,
-    use: 'node-loader',
+    use: "node-loader",
   },
   {
     test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
+      loader: "@vercel/webpack-asset-relocator-loader",
       options: {
-        outputAssetBase: 'native_modules',
+        outputAssetBase: "native_modules",
       },
     },
   },
@@ -22,18 +22,23 @@ module.exports = [
     test: /\.jsx?$/,
     exclude: /node_modules/,
     use: {
-      loader: 'babel-loader',
+      loader: "babel-loader",
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react']
-      }
-    }
+        presets: ["@babel/preset-env", "@babel/preset-react"],
+      },
+    },
   },
   {
     // Loads .css files
     test: /\.css$/,
     include: [path.resolve(__dirname, "app/src")],
-    use: ["style-loader", "css-loader", "postcss-loader"]
-  }
+    use: ["style-loader", "css-loader", "postcss-loader"],
+  },
+  {
+    // loads images
+    test: /\.(png|svg|jpg)$/i,
+    type: "asset/resource",
+  },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
