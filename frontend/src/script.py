640b1cb3cbaadeaa96e4cfe2bb40e5ea0ab39e3b
Obaida Almelhem <almelhemo@gmail.com>
2025-01-25 10:08:06 +0300

    reading more data from python, normalizing the arabic text, added a function to help display arabic text when debugging

--- frontend/src/script.py
+++ frontend/src/script.py
@@ -3,6 +3,27 @@ import pandas as pd
 from bidi.algorithm import get_display
 import sys
 import json
+import unicodedata
+
+def normalize_arabic_text(text):
+    """
+    normalizes the arabic text to avoid font rendering or character encoding issues
+    """
+    return unicodedata.normalize('NFKC', text)
+
+def decode_unicode(input_json):
+    """
+    Decode Unicode-encoded strings in a JSON object.
+    use when debugging cause it displays the arabic text instead of the unicoded characters 
+
+    Args:
+        input_json (str): JSON string with Unicode characters.
+
+    Returns:
+        str: Decoded JSON string with readable Arabic text.
+    """
+    decoded = json.loads(input_json)
+    return json.dumps(decoded, ensure_ascii=False, indent=4)
 
 
 def extract_table_from_pdf(pdf_path):
@@ -49,9 +70,10 @@ def process_table_to_json(df: pd.DataFrame):
         try:
             price = str(row.iloc[8]).replace(",", "")
             name = row.iloc[13]
+            name = normalize_arabic_text(name[::-1])
             # Create a product dictionary
             product = {
-                "name": name[::-1],
+                "name": name,
                 "total_count": int(row.iloc[10]),
                 "price": int(float(price)),
             }
@@ -85,25 +107,49 @@ def extract_invoice_details(pdf_path):
             # Split text into lines
             lines = text.split("\n")
 
-            # Find the line containing "البيان"
-            start = ":نايبلا"
+            # Find the line containing important data
+            date_start = ':خيراتلا'
+            pharmacist_start = ':ديسلا'
+            statement_start = ":نايبلا"
+
+            date = ''
+            pharmacist = ''
+            statement = ''
+            id = 0
 
             storage = "mo"
             for line in lines:
                 if "SUB-ADvanced" in line:
                     storage = "ad"
-                if start in line:
-                    parts = line.split(" ")
-                    # Assume the ID is the first numeric value, and the rest is the pharmacist name
+
+                if statement_start in line:
+                    parts = line.split(" ") 
                     id = next((part for part in parts if part.isdigit()), "")
-                    name = " ".join(
-                        part for part in parts if (part != id and part != start)
+                    statement = " ".join(
+                        part for part in parts if (part != id and part != statement_start)
                     )
-                    return {
-                        "id": int(id),
-                        "storage": storage,
-                        "pharmacist": name[::-1].strip(),
-                    }
+
+                if pharmacist_start in line:
+                    sections = line.split(date_start)
+                    pharmacist_parts = sections[1].split(" ") 
+                    pharmacist = " ".join(
+                        part for part in pharmacist_parts if (part != id and part != pharmacist_start)
+                    )
+                    date_parts = sections[0].split(" ") 
+                    date = " ".join(
+                        part for part in date_parts if (part != id and part != pharmacist_start)
+                    )
+            
+            statement = normalize_arabic_text(statement[::-1].strip())
+            pharmacist = normalize_arabic_text(pharmacist[::-1].strip())
+            
+            return {
+                "id": int(id),
+                "storage": storage,
+                "statement": statement,
+                "pharmacist": pharmacist,
+                "date": date.strip()
+            }
     return {"err": "invalid request"}
 
 
@@ -129,14 +175,18 @@ def main():
         # Combine details and items into final JSON
         output = {
             "id": details["id"],
-            "pharmacist": details["pharmacist"],
+            "statement": details["statement"],
             "storage": details["storage"],
+            "pharmacist": details["pharmacist"],
+            "date": details["date"],
             "items": items,
         }
+        # print(decode_unicode(json.dumps(output)))
         print(json.dumps(output))
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred:")
+        raise e
 
 
 if __name__ == "__main__":
