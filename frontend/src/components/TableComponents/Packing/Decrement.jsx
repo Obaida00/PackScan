0ccdb69e40a67fd298d66916c12d3b03dcc7e0e4
Obaida Almelhem <almelhemo@gmail.com>
2025-01-28 13:20:41 +0300

    some designing changes to the packing table

--- frontend/src/components/TableComponents/Packing/Decrement.jsx
+++ frontend/src/components/TableComponents/Packing/Decrement.jsx
@@ -2,9 +2,9 @@ import * as React from "react";
 
 function Decrement({ data, action }) {
   return (
-    <div className="w-full flex">
+    <div className="w-full flex justify-center">
       <button
-        className="w-full rounded-full bg-red-500 hover:bg-red-700 opacity-40 hover:opacity-100 font-serif font-black transition-all duration-100"
+        className="w-[24px] rounded-full bg-red-500 hover:bg-red-700 opacity-0 group-hover:opacity-70 font-serif font-black transition-all duration-200"
         onClick={() => action(data.barcode)}
       >
         -
