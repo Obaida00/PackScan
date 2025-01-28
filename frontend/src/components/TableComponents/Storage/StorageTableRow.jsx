b9f1d9391efad858cdded57c57f70bb8afb94336
Obaida Almelhem <almelhemo@gmail.com>
2025-01-28 12:21:46 +0300

    redesigning storage invoice modal using material ui & removed auto opening cause it will make stupid stuff with the auto focus to the packer id field

--- frontend/src/components/TableComponents/Storage/StorageTableRow.jsx
+++ frontend/src/components/TableComponents/Storage/StorageTableRow.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import StatusBadge from "/src/components/StatusBadge.jsx";
 import TableDataCell from "../TableDataCell.jsx";
-import InvoiceModal from "./StorageInvoiceModal.jsx";
+import StorageInvoiceModal from "./StorageInvoiceModal.jsx";
 
-function StorageTableRow({ i, invoice, openModal }) {
+function StorageTableRow({ i, invoice }) {
   return (
-    <tr className=" border-b bg-gray-800 border-gray-700 hover:bg-gray-600 transition ease-in-out text-center">
+    <tr className="border-b bg-gray-800 border-gray-700 hover:bg-gray-600 transition ease-in-out text-center">
       <TableDataCell key={i} data={invoice.id} />
 
       <td>
         <StatusBadge badgeName={invoice.status} />
       </td>
-      <td className="px-6 py-4 text-right">
-        <InvoiceModal key={i} invoice={invoice} openModal={openModal}/>
+      <td>
+        <StorageInvoiceModal key={i} invoice={invoice} />
       </td>
     </tr>
   );
