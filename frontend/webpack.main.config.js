0e44b799f0d492da9f5c51af67bf7e1a004cc97b
Obaida Almelhem <almelhemo@gmail.com>
2025-02-04 23:05:52 +0300

    added python barcode generator script to the webpack copy plugin

--- frontend/webpack.main.config.js
+++ frontend/webpack.main.config.js
@@ -15,6 +15,7 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         { from: path.resolve(__dirname, "./src/process_invoice_pdf.py"), to: "./" },
+        { from: path.resolve(__dirname, "./src/generate_barcode.py"), to: "./" },
         { from: path.resolve(__dirname, "./src/assets/sounds/scannerBeep.mp3"), to: "./sounds" },
         { from: path.resolve(__dirname, "./src/assets/sounds/error.mp3"), to: "./sounds" },
         { from: path.resolve(__dirname, "./src/assets/sounds/complete.mp3"), to: "./sounds" },
