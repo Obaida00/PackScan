3838eb50b786519719e1bf1fbdca7f78da934329
Obaida Almelhem <almelhemo@gmail.com>
2025-02-07 11:13:35 +0300

    some desing refactoring on storage index page

--- frontend/src/views/StorageIndex.jsx
+++ frontend/src/views/StorageIndex.jsx
@@ -62,34 +62,32 @@ function StorageIndex({ storageIndex }) {
   return (
     <>
       <div>
-        <div className="flex justify-center">
-          <div className="flex justify-between w-[90%]">
-            {/* Back Button */}
-            <div className="flex justify-center py-4">
-              <div className="min-h-[5vh] w-[90%] flex justify-start">
-                <BackButton />
-              </div>
+        <div className="flex justify-between w-full px-5 py-4">
+          {/* Back Button */}
+          <div className="flex justify-center]">
+            <div className="min-h-[5vh] w-full flex justify-start">
+              <BackButton />
             </div>
-            {/* Logs */}
-            <div className="flex justify-center py-4">
-              <div className="min-h-[5vh] w-[90%] flex justify-end">
-                <Link
-                  to={`logs`}
-                  type="button"
-                  className="text-slate-400 hover:text-slate-100 border border-slate-400 hover:bg-slate-700 focus:ring-2 focus:outline-none focus:ring-slate-300 font-semibold rounded-xl text-sm px-4 py-2 text-center flex items-center transition-all duration-75 group/start"
+          </div>
+          {/* Logs */}
+          <div className="flex justify-center]">
+            <div className="min-h-[5vh] w-full flex justify-end">
+              <Link
+                to={`logs`}
+                type="button"
+                className="text-slate-400 hover:text-slate-100 border border-slate-400 hover:bg-slate-700 focus:ring-2 focus:outline-none focus:ring-slate-300 font-semibold rounded-xl text-sm px-4 py-2 text-center flex items-center transition-all duration-75 group/start"
+              >
+                <p>Logs</p>
+                &nbsp;
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="20px"
+                  viewBox="0 -960 960 960"
+                  className="fill-slate-500 group-hover/start:fill-slate-200"
                 >
-                  <p>Logs</p>
-                  &nbsp;
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="20px"
-                    viewBox="0 -960 960 960"
-                    className="fill-slate-500 group-hover/start:fill-slate-200"
-                  >
-                    <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-                  </svg>
-                </Link>
-              </div>
+                  <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+                </svg>
+              </Link>
             </div>
           </div>
         </div>
