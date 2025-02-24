d6bb54b8e306f5f763c2668b08e2ae81f274d7bb
Obaida Almelhem <almelhemo@gmail.com>
2025-02-24 13:39:51 +0300

    made the row for the storage table changable ( passable ) and added new permission components

--- frontend/src/views/StorageIndex/components/StorageTable.jsx
+++ frontend/src/views/StorageIndex/components/StorageTable.jsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import TableHeader from "../../../shared/components/TableComponents/TableHeader.jsx";
-import TableRow from "./StorageTableRow.jsx";
 import TableEmptyRow from "../../../shared/components/TableComponents/TableEmptyRow.jsx";
 
-function StorageTable({ data }) {
+function StorageTable({ data, RowComponent }) {
   return (
     <table className="w-full text-md text-gray-300 tracking-wide ">
       <thead className="text-mg bg-gray-700 text-gray-400">
@@ -15,7 +14,9 @@ function StorageTable({ data }) {
       </thead>
       <tbody>
         {data.length > 0 ? (
-          data.map((obj, key) => <TableRow key={key} i={key} invoice={obj} />)
+          data.map((obj, key) => (
+            <RowComponent key={key} i={key} invoice={obj} />
+          ))
         ) : (
           <TableEmptyRow />
         )}
