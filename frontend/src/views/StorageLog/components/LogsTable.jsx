8afcd30804b0de6f0d322ea7b40ea530cec3426c
Obaida Almelhem <almelhemo@gmail.com>
2025-03-01 12:51:51 +0300

    apply primary id changing to storage log view

--- frontend/src/views/StorageLog/components/LogsTable.jsx
+++ frontend/src/views/StorageLog/components/LogsTable.jsx
@@ -3,28 +3,27 @@ import TableHeader from "../../../shared/components/TableComponents/TableHeader.
 import LogsTableRow from "./LogsTableRow.jsx";
 import LogsTableRowForMissingInvoices from "./LogsTableRowForMissingInvoices.jsx";
 
-function LogsTable({ data, minId, maxId, reloadInvoices }) {
-  // Create a full range of IDs for the current page
-  const fullData = Array.from({ length: maxId - minId + 1 }, (_, i) => {
-    const id = maxId - i;
-    return data.find((obj) => obj.id === id) || { missing: id }; // Use null for missing IDs
-  });
-
+function LogsTable({ invoices, reloadInvoices }) {
   return (
     <table className="w-full text-md text-gray-300 tracking-wide ">
       <thead className="text-mg bg-gray-700 text-gray-400">
         <tr>
-          <TableHeader columnName="Package ID" />
+          <TableHeader columnName="Invoice ID" />
           <TableHeader columnName="Status" />
           <TableHeader columnName="" />
         </tr>
       </thead>
       <tbody>
-        {fullData.map((obj, key) =>
-          obj["missing"] !== undefined ? (
-            <LogsTableRowForMissingInvoices key={key} id={obj["missing"]} />
+        {invoices.map((invoice, key) =>
+          false ? ( // for future missing invoices handling
+            <LogsTableRowForMissingInvoices key={key} id={0} />
           ) : (
-            <LogsTableRow key={key} i={key} invoice={obj} reloadInvoices={reloadInvoices} />
+            <LogsTableRow
+              key={key}
+              i={key}
+              invoice={invoice}
+              reloadInvoices={reloadInvoices}
+            />
           )
         )}
       </tbody>
