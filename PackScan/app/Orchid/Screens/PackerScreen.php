284a19651af1309024d6234810ee73d4f66c4d4b
Obaida Almelhem <almelhemo@gmail.com>
2025-03-03 21:22:47 +0300

    added editing functionality to packer screen and refactored code & added packer repository & added fa icons

--- PackScan/app/Orchid/Screens/PackerScreen.php
+++ PackScan/app/Orchid/Screens/PackerScreen.php
@@ -5,6 +5,7 @@ namespace App\Orchid\Screens;
 use App\Models\Packer;
 use Illuminate\Http\Request;
 use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\DropDown;
 use Orchid\Screen\Actions\ModalToggle;
 use Orchid\Screen\Fields\CheckBox;
 use Orchid\Screen\Fields\Input;
@@ -14,6 +15,11 @@ use Orchid\Support\Facades\Layout;
 
 class PackerScreen extends Screen
 {
+    /**
+     * @var Packer|null
+     */
+    public $packer = null;
+
     /**
      * Fetch data to be displayed on the screen.
      *
@@ -46,7 +52,8 @@ class PackerScreen extends Screen
         return [
             ModalToggle::make('Add Packer')
                 ->modal('packerModal')
-                ->method('create')
+                ->modalTitle('Create Packer')
+                ->method('save')
                 ->icon('plus'),
         ];
     }
@@ -62,44 +69,105 @@ class PackerScreen extends Screen
             Layout::table('packers', [
                 TD::make('id'),
                 TD::make('name'),
-                TD::make('can_manually_submit', 'Can Manually Submit'),
-                TD::make('can_submit_important_invoices', 'Can Submit Important Invoices'),
+                TD::make('Can Manually Submit')
+                    ->render(function (Packer $packer) {
+                        return $packer->can_manually_submit ? 'YES' : 'NO';
+                    }),
+                TD::make('Can Submit Important Invoices')
+                    ->render(function (Packer $packer) {
+                        return $packer->can_submit_important_invoices ? 'YES' : 'NO';
+                    }),
                 TD::make('Actions')
                     ->alignRight()
                     ->render(function (Packer $packer) {
-                        return Button::make('Delete Packer')
-                            ->confirm("Are you sure you want to delete this packer? - name: {$packer->name}")
-                            ->method('delete', ['packer' => $packer->id]);
+                        return DropDown::make()
+                            ->icon('fa.chevron-down')
+                            ->list([
+                                ModalToggle::make('Edit Packer')
+                                    ->icon('fa.pen')
+                                    ->modal('packerModal')
+                                    ->modalTitle('Edit Packer: ' . $packer->name)
+                                    ->method('save')
+                                    ->asyncParameters([
+                                        'packer' => $packer->id,
+                                    ]),
+                                Button::make('Delete Packer')
+                                    ->icon('fa.trash')
+                                    ->confirm("Are you sure you want to delete this packer? - name: {$packer->name}")
+                                    ->method('delete', ['packer' => $packer->id])
+                            ])
+                            ->style('background-color: black; color: white; border-radius: 10px; padding: 5px 10px');
                     }),
             ]),
 
             Layout::modal('packerModal', Layout::rows([
                 Input::make('packer.id')
                     ->title('Id')
-                    ->placeholder('Enter packer ID'),
+                    ->placeholder('Enter packer ID')
+                    ->required(),
                 Input::make('packer.name')
                     ->title('Name')
-                    ->placeholder('Enter packer name'),
+                    ->placeholder('Enter packer name')
+                    ->required(),
                 CheckBox::make('packer.can_manually_submit')
                     ->title('Can Manually Submit')
-                    ->placeholder('Permission to manually submit invoices')->sendTrueOrFalse(),
+                    ->placeholder('Permission to manually submit invoices')
+                    ->sendTrueOrFalse(),
                 CheckBox::make('packer.can_submit_important_invoices')
                     ->title('Can Submit Important Invoices')
-                    ->placeholder('Permission to submit important invoices')->sendTrueOrFalse(),
+                    ->placeholder('Permission to submit important invoices')
+                    ->sendTrueOrFalse(),
+                Input::make('packer_id')
+                    ->type('hidden')
             ]))
-                ->title('Create Packer')
-                ->applyButton('Add Packer'),
+                ->async('asyncGetPacker')
+                ->applyButton('Save Packer'),
         ];
     }
 
+    /**
+     * @param int|null $packer
+     *
+     * @return array
+     */
+    public function asyncGetPacker(?int $packer = null): array
+    {
+        if (!$packer) {
+            return [
+                'packer' => [
+                    'id' => '',
+                    'name' => '',
+                    'can_manually_submit' => false,
+                    'can_submit_important_invoices' => false,
+                ],
+                'packer_id' => null,
+            ];
+        }
+
+        $packerModel = Packer::findOrFail($packer);
+
+        return [
+            'packer' => [
+                'id' => $packerModel->id,
+                'name' => $packerModel->name,
+                'can_manually_submit' => $packerModel->can_manually_submit,
+                'can_submit_important_invoices' => $packerModel->can_submit_important_invoices,
+            ],
+            'packer_id' => $packerModel->id,
+        ];
+    }
 
     /**
      * @param \Illuminate\Http\Request $request
      *
      * @return void
      */
-    public function create(Request $request)
+    public function save(Request $request)
     {
+        $packer_id = $request->input('packer_id');
+        $isEdit = !empty($packer_id);
+
+        // Validate the input data
         $request->validate([
             'packer.id' => 'required|integer',
             'packer.name' => 'required|string|max:255',
@@ -107,14 +175,29 @@ class PackerScreen extends Screen
             'packer.can_submit_important_invoices' => 'required|boolean'
         ]);
 
-        $packer = Packer::create([
+        $packerData = [
             'id' => $request->input('packer.id'),
             'name' => $request->input('packer.name'),
             'can_manually_submit' => $request->input('packer.can_manually_submit'),
             'can_submit_important_invoices' => $request->input('packer.can_submit_important_invoices')
-        ]);
+        ];
 
-        $packer->save();
+        if ($isEdit) {
+            $packer = Packer::findOrFail($packer_id);
+
+            // Check if ID has changed
+            if ($packer->id != $packerData['id']) {
+                // Delete the old record and create a new one with the updated ID
+                $packer->delete();
+                Packer::create($packerData);
+            } else {
+                // Normal update when ID hasn't changed
+                $packer->update($packerData);
+            }
+        } else {
+            // Create a new packer
+            Packer::create($packerData);
+        }
     }
 
     /**
