db515365d3421695beb2c5c70b3f2ec1bb17b791
Obaida Almelhem <almelhemo@gmail.com>
2025-03-21 11:58:28 +0300

    made collection names unique in database

--- PackScan/database/migrations/2025_03_18_202834_create_collections_table.php
+++ PackScan/database/migrations/2025_03_18_202834_create_collections_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('collections', function (Blueprint $table) {
             $table->uuid('id')->primary();
 
-            $table->string('name');
+            $table->string('name')->unique();
             $table->foreignId('storage_id')->nullable()->constrained('storages')->onDelete('cascade');
 
             $table->timestamps();
