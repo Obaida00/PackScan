a077a3c0caaca1a216131006ea239a47e5a4c10e
Obaida Almelhem <almelhemo@gmail.com>
2025-03-30 16:57:56 +0300

    add logo to home navigation from header logo

--- frontend/src/views/Layout/components/Header.jsx
+++ frontend/src/views/Layout/components/Header.jsx
@@ -12,11 +12,18 @@ function Header() {
     navigate("/settings");
   };
 
+  const handleLogoClick = () => {
+    navigate("/");
+  };
+
   return (
     <nav className="max-h-[10vh] bg-slate-500 dark:bg-gray-800">
       <div className="max-w-screen-xl flex justify-between mx-auto p-[2vh]">
-        <div className="flex space-x-2">
-          <img className="h-[6vh]" alt="" src={logoPath}></img>
+        <div
+          className="flex space-x-2 cursor-pointer"
+          onClick={handleLogoClick}
+        >
+          <img className="h-[6vh]" alt="logo" src={logoPath}></img>
           <p className="self-center text-2xl whitespace-nowrap font-cocon text-white">
             PackScan
           </p>
