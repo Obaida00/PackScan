f185b6be29183dd9a5ae5a614688e02a96df4fb4
Obaida Almelhem <almelhemo@gmail.com>
2025-03-31 18:34:19 +0300

    added Storages Collections Products admin panels

---
+++ PackScan/app/Filament/Resources/CollectionResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CollectionResource\Pages;
+use App\Models\Collection;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CollectionResource extends Resource
+{
+    protected static ?string $model = Collection::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-folder-open';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->unique('collections', 'name')
+                    ->label('Name'),
+                Select::make('storage_id')
+                    ->relationship('storage', 'name')
+                    ->required()
+                    ->label('Storage'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('storage.name')
+                    ->label('Storage')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('products_count')
+                    ->counts('products')
+                    ->label('Products'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCollections::route('/'),
+            'create' => Pages\CreateCollection::route('/create'),
+            'edit' => Pages\EditCollection::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ PackScan/app/Filament/Resources/ProductResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Models\Product;
+use Filament\Forms\Form;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cube';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->unique('products', 'name')
+                    ->label('Name'),
+                TextInput::make('barcode')
+                    ->required()
+                    ->numeric()
+                    ->unique('products', 'barcode')
+                    ->label('Barcode'),
+                Select::make('collection_id')
+                    ->relationship('collection', 'name')
+                    ->required()
+                    ->label('Collection'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('barcode')
+                    ->label('Barcode')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('collection.name')
+                    ->label('Collection')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
