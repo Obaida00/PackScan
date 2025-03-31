b7aeba494b38d2a168c52651274bd62b3825714f
Obaida Almelhem <almelhemo@gmail.com>
2025-03-31 18:38:44 +0300

    added Packers admin panels

---
+++ PackScan/app/Filament/Resources/PackerResource/Pages/CreatePacker.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\PackerResource\Pages;
+
+use App\Filament\Resources\PackerResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePacker extends CreateRecord
+{
+    protected static string $resource = PackerResource::class;
+}
