55bc8155bc93b25ba76ca3bcc8528496ab011a80
Obaida Almelhem <almelhemo@gmail.com>
2025-04-02 20:25:38 +0300

    getting only pending invoices in storage index

--- frontend/src/views/StorageIndex/StorageIndex.jsx
+++ frontend/src/views/StorageIndex/StorageIndex.jsx
@@ -44,6 +44,7 @@ function StorageIndex() {
         isImportant: true,
         isMissing: false,
         pageNumber: 1,
+        status: 'Pending'
       };
       const data = await ipcRenderer.invoke("fetch-orders", filters);
       setImportantInvoices(data?.data || []);
@@ -69,6 +70,7 @@ function StorageIndex() {
         isImportant: false,
         isMissing: false,
         pageNumber: 1,
+        status: 'Pending'
       };
       const data = await ipcRenderer.invoke("fetch-orders", filters);
 
