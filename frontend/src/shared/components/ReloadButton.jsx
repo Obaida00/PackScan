e5e250ec3f7b9fd011c84c53274b86aa6511a32e
Obaida Almelhem <almelhemo@gmail.com>
2025-04-02 23:39:54 +0300

    fix bug localization function not imported

--- frontend/src/shared/components/ReloadButton.jsx
+++ frontend/src/shared/components/ReloadButton.jsx
@@ -1,11 +1,19 @@
 import { Button, Tooltip } from "antd";
 import * as React from "react";
-import {RedoOutlined} from "@ant-design/icons";
+import { RedoOutlined } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
 
 function ReloadButton({ callback }) {
+  const { t } = useTranslation();
+
   return (
     <Tooltip title={t("common.refresh")}>
-      <Button shape="circle" size="large" icon={<RedoOutlined/>} onClick={async () => await callback()} />
+      <Button
+        shape="circle"
+        size="large"
+        icon={<RedoOutlined />}
+        onClick={async () => await callback()}
+      />
     </Tooltip>
   );
 }
