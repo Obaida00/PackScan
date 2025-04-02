3a85139cfe12fec7bfa87c4182c343cc0a42aa65
Obaida Almelhem <almelhemo@gmail.com>
2025-04-02 20:24:12 +0300

    fix arrowIcon rtl/ltr rotating not working properly

--- frontend/src/shared/components/ArrowIcon.jsx
+++ frontend/src/shared/components/ArrowIcon.jsx
@@ -7,8 +7,8 @@ function ArrowIcon({ height, groupName, reversed = false }) {
       height={height}
       viewBox="0 -960 960 960"
       className={`fill-slate-500 group-hover/${groupName}:fill-slate-200 ${
-        reversed ? "ltr" : "rtl"
-      }:rotate-180`}
+        reversed ? "rotate-180 rtl:rotate-0" : "rtl:rotate-180"
+      }`}
     >
       <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
     </svg>
