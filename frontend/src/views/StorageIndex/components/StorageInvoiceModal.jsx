0b851e573179841fcc12b35ecc02457742f9c1be
talalcodes55 <talalalghazal55@gmail.com>
2025-04-05 23:29:19 +0300

    removed unused logic

--- frontend/src/views/StorageIndex/components/StorageInvoiceModal.jsx
+++ frontend/src/views/StorageIndex/components/StorageInvoiceModal.jsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import "../../../shared/styles/Modal.css";
 import { useNavigate } from "react-router-dom";
 import TextField from "@mui/material/TextField";
-import { Button, Modal } from "antd";
+import { Button, Modal, Form, Input } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -13,8 +14,7 @@ import { useTranslation } from "react-i18next";
 function StorageInvoiceModal({ invoice }) {
   const { t } = useTranslation();
   const [open, setOpen] = useState(false);
-  const [packerName, setPackerName] = useState("");
-  const [packerFieldError, setPackerFieldError] = useState(false);
+  
   const nav = useNavigate();
 
   const rows = [
@@ -27,7 +27,6 @@ function StorageInvoiceModal({ invoice }) {
 
   useEffect(() => {
     setPackerById("");
-    setPackerFieldError(false);
   }, [open]);
 
   const setPackerById = async (id) => {
@@ -37,36 +36,28 @@ function StorageInvoiceModal({ invoice }) {
       id.toString().trim() === "" ||
       !/^-?\d{4,}$/.test(id)
     ) {
-      setPackerName("");
       return;
     }
 
     let packer = await ipcRenderer.invoke("fetch-packer", id);
     if (packer.length === 0) {
-      setPackerName("");
       return;
     }
-    setPackerName(packer.name);
-    setPackerFieldError(false);
   };
 
   const submitForm = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const formJson = Object.fromEntries(formData.entries());
-    const id = formJson.id;
+    const id = event.id;
     setPackerById(id);
 
-    if (packerName !== "") {
-      handleClose();
-      nav(`/packing/${invoice.id}`, {
-        state: { packerId: id },
-      });
-    } else {
-      setPackerFieldError(true);
-    }
+    ipcRenderer.invoke("fetch-packer", id).then((packer) => {
+      if (packer.id) {
+        handleClose();
+        nav(`/packing/${invoice.id}`, {
+          state: { packerId: id },
+        });
+      }
+    });
   };
-
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -96,158 +87,48 @@ function StorageInvoiceModal({ invoice }) {
       <Modal
         title={`-${invoice.invoice_id}-`}
         open={open}
-        onOk={handleClose}
+        onCancel={handleClose}
+        footer={[
+          <Form onFinish={submitForm}>
+            <Form.Item
+              label={t("packer.title")}
+              name="id"
+              rules={[{ required: true, message: "Please enter ID" }]}
+            >
+              <Input placeholder={t("packer.packerIdPlaceholder")} id="name" />
+            </Form.Item>
+            <Button
+              htmlType="submit"
+              variant="solid"
+              color="green"
+              icon={<ArrowRightOutlined />}
+            >
+              {t("invoice.startPacking")}
+            </Button>
+          </Form>,
+        ]}
       >
         <table className="table-auto text-start w-full">
           <tbody>
             {rows.map((row, index) => (
-              <tr key={index} className="border-b-2 border-slate-50">
-                <td className="py-1 w-48 font-medium text-sm text-gray-500">
+              <tr key={index} className="border-b-1 border-slate-50">
+                <td className="py-1 font-normal dark:text-gray-300 text-gray-500">
                   {t("invoice." + row.label)}
                 </td>
-                <td className="py-1 w-[300px] font-semibold text-lg text-slate-900">
+                <td className="py-1 font-medium text-xl text-slate-800 dark:text-slate-100">
                   {row.value}
                 </td>
               </tr>
             ))}
-            <tr className="border-b-2 border-slate-50">
-              <td className="py-2 w-48 font-medium text-gray-500">
-                {t("packer.title")}
-              </td>
+            <tr className="border-b dark:border-slate-50 border-slate-400">
+              <td className="py-2 w-48 font-medium text-gray-500"></td>
               <td className="w-64 py-2 text-xl text-slate-900">
-                <div className="flex gap-2 items-center ">
-                  <div className="w-[100px]">
-                    <TextField
-                      autoFocus
-                      required
-                      error={packerFieldError}
-                      size="small"
-                      margin="none"
-                      id="name"
-                      name="id"
-                      placeholder={t("packer.packerIdPlaceholder")}
-                      variant="outlined"
-                      onChange={(e) => setPackerById(e.target.value)}
-                    />
-                  </div>
-                  <div
-                    className={`w-[200px] overflow-hidden text-ellipsis whitespace-nowrap font-medium ${
-                      packerFieldError
-                        ? "text-red-700 text-xs opacity-80"
-                        : "text-slate-800 text-lg"
-                    }`}
-                  >
-                    {packerFieldError ? t("packer.idNotValid") : packerName}
-                  </div>
-                </div>
+                <div className="flex gap-2 items-center "></div>
               </td>
             </tr>
           </tbody>
         </table>
       </Modal>
-      {/* <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: "form",
-          onSubmit: submitForm,
-        }}
-        className="text-center"
-        fullWidth
-      >
-        <DialogTitle className="flex align-middle justify-between p-3 bg-slate-200">
-          <div className="text-3xl font-mono font-semibold text-gray-800">
-            -{invoice.invoice_id}-
-          </div>
-          <button
-            onClick={handleClose}
-            type="button"
-            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm inline-flex justify-center items-center transition-colors duration-300 w-[24px]"
-          >
-            <svg
-              className="w-3 h-3"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
-          </button>
-        </DialogTitle>
-        <DialogContent>
-          <div className="p-2">
-            <table className="table-auto text-start w-full">
-              <tbody>
-                {rows.map((row, index) => (
-                  <tr key={index} className="border-b-2 border-slate-50">
-                    <td className="py-1 w-48 font-medium text-sm text-gray-500">
-                      {t("invoice." + row.label)}
-                    </td>
-                    <td className="py-1 w-[300px] font-semibold text-lg text-slate-900">
-                      {row.value}
-                    </td>
-                  </tr>
-                ))}
-                <tr className="border-b-2 border-slate-50">
-                  <td className="py-2 w-48 font-medium text-gray-500">
-                    {t("packer.title")}
-                  </td>
-                  <td className="w-64 py-2 text-xl text-slate-900">
-                    <div className="flex gap-2 items-center ">
-                      <div className="w-[100px]">
-                        <TextField
-                          autoFocus
-                          required
-                          error={packerFieldError}
-                          size="small"
-                          margin="none"
-                          id="name"
-                          name="id"
-                          placeholder={t("packer.packerIdPlaceholder")}
-                          variant="outlined"
-                          onChange={(e) => setPackerById(e.target.value)}
-                        />
-                      </div>
-                      <div
-                        className={`w-[200px] overflow-hidden text-ellipsis whitespace-nowrap font-medium ${
-                          packerFieldError
-                            ? "text-red-700 text-xs opacity-80"
-                            : "text-slate-800 text-lg"
-                        }`}
-                      >
-                        {packerFieldError ? t("packer.idNotValid") : packerName}
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <button
-            className="text-slate-100 border border-slate-400 bg-green-600 hover:bg-green-700 focus:ring-2 focus:outline-none focus:ring-slate-300  font-semibold rounded-xl text-sm px-4 py-2 text-center flex items-center transition-all duration-200"
-            type="submit"
-          >
-            <p>{t("invoice.startPacking")}</p>
-            &nbsp;
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="20px"
-              viewBox="0 -960 960 960"
-              className="fill-slate-200"
-            >
-              <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-            </svg>
-          </button>
-        </DialogActions>
-      </Dialog> */}
     </>
   );
 }
